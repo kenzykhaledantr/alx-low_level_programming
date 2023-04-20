e9f0fad8507becce029ae71dead2ff578349a091
kenzykhaledantr <kkh162003@gmail.com>
2023-04-20 08:28:26 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,9 @@
 /**
 * main - Entry point
 *
+* Description: Prints the string "with proper grammar, but the outcome is a
+* piece of art," followed by a new line, using the function printf.
+*
 * Return : Always 0 (Success)
 */
 int main(void)
