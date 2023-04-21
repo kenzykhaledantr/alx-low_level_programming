0e70f966b65b260478daf8863d3c223741955bdf
kenzykhaledantr <kkh162003@gmail.com>
2023-04-21 10:21:21 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,12 +15,12 @@ int main(void)
 		{
 			for (l = 50; l < 58; l++)
 			{
-				if(l > m && m > n)
+				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
 					putchar(l);
-					if (n != 56 || m != 57)
+					if (n != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
