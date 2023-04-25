a0c03c9599ac16910f6afb5a7430bff5ad93edcf
kenzykhaledantr <kkh162003@gmail.com>
2023-04-25 16:13:10 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - fibonacci <3
- *
- * Purpose - no hardcode
- * Return: (Success) 
- */
+* main - fibonacci <3
+*
+* Purpose - no hardcode
+*
+* Return:  (Success)
+*/
 int main(void)
 {
 unsigned long int i;
